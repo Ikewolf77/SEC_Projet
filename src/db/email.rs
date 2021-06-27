a8b70caacfd3cdd498f168cc0c9980e511c9ee89
Simon <simon.mattei@heig-vd.ch>
2021-06-27 21:50:33 +0200

    fini

--- src/db/email.rs
+++ src/db/email.rs
@@ -1,14 +1,9 @@
 use lettre::transport::smtp::authentication::Credentials;
 use lettre::{Message, SmtpTransport, Transport};
 
-const SMTP_USER: &str = "simon.scoolman@gmail.com";
-const SMTP_PASS: &str = "ioghrmtlcgqgrrsf";
-const SMTP_SERV: &str = "smtp.gmail.com";
-
-/*
 const SMTP_USER: &str = "USER";
 const SMTP_PASS: &str = "PASS";
-const SMTP_SERV: &str = "SERV";*/
+const SMTP_SERV: &str = "SERV";
 
 pub fn send_password_mail(dst: &str, code: &str) {
     let email = Message::builder()
